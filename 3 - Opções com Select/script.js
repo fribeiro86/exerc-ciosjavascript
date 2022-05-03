2cf5342301d4978326d1b696e78fd7f91bd9352b
fabricioribeiro <dfabriosp@gmail.com>
2022-05-02 22:56:34 -0300

    Criação do formulário cliente

--- 3 - Opções com Select/script.js
+++ 3 - Opções com Select/script.js
@@ -1,5 +1,7 @@
 function changeColor() {
 
     let cores = document.getElementById("colors").value;
-    document.body.style.backgroundColor = cores;     
+    document.body.style.backgroundColor = cores; 
+    
+  
 }
