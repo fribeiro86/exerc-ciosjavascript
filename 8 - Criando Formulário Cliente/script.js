93ca9d7895a1081684e5ee395899a67e0569c145
fabricioribeiro <dfabriosp@gmail.com>
2022-05-03 18:55:39 -0300

    Formulário cliente primeira versão

--- 8 - Criando Formulário Cliente/script.js
+++ 8 - Criando Formulário Cliente/script.js
@@ -1,4 +1,36 @@
-function alertar() {
 
-    alert("porra");
+
+window.addEventListener('load', () => {
+
+  document.getElementById("cpf").disabled= true;
+  document.getElementById("cnpj").disabled = true;
+  document.getElementById("dataNascimento").disabled = true;
+ 
+   
+ 
+
+})
+
+function verificarStatus() {
+
+    let pessoaFisica = document.getElementById("fisica").checked; 
+    let pessoaJuridica = document.getElementById("juridica").checked; 
+
+    if(pessoaFisica){
+
+        document.getElementById("cpf").disabled= false;
+        document.getElementById("cnpj").disabled = true;
+        document.getElementById("dataNascimento").disabled = false;
+    }
+    
+    if(pessoaJuridica){
+
+        document.getElementById("cnpj").disabled = false;
+        document.getElementById("cpf").disabled= true;
+        document.getElementById("dataNascimento").disabled = true;
+        document.getElementById("cpf").value = "";
+        document.getElementById("dataNascimento").value = ""; 
+
+    }
+
 }
